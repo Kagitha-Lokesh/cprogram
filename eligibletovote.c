bf1a81bfabb0eb7f0abae5260af59003e0822b89
Kagitha-Lokesh <kagithalokesh17@gmail.com>
2024-10-21 11:37:02 +0530

    Create eligibletovote.c

---
+++ eligibletovote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter the age of the persion : ");
+scanf("%d",&a);
+if(a>=18)
+{
+printf("The persion is eligible to vote.");
+}
+else{
+printf("The persion is not eligible to vote.");
+}
+
+}
